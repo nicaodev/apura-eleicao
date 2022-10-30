05849c7081f0e3bd1bc23b8ffd2800b6dd11630f
Nicaodev <alexandre.nicos@gmail.com>
2022-10-30 17:40:06 -0300

    ajustes para 2o turno.

--- apura-eleicao.py
+++ apura-eleicao.py
@@ -1,25 +1,33 @@
 import requests
 import json
 import pandas as pd
+import os
+from time import sleep
+from datetime import datetime
 
-data = requests.get(
-    'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+while (True): 
 
-data = json.loads(data.content)
+    data = requests.get(
+       # 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json') # 1o turno
+       'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json') # 2o turno
 
-candidato = []
-votos = []
-porcentagem = []
-procentagemUrnas = data['pst']
+    data = json.loads(data.content)
 
-for info in data['cand']:
+    candidato = []
+    votos = []
+    porcentagem = []
+    procentagemUrnas = data['pst']
 
-    if info['seq'] == '1' or info['seq']== '2' or info['seq'] =='4':
-        candidato.append(info['nm'])
-        votos.append(info['vap'])
-        porcentagem.append(info['pvap'])
+    for info in data['cand']:
 
-df = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=['Candidato', 'N votos', 'Porcentagem'])
+        if info['seq'] == '1' or info['seq']== '2' or info['seq'] =='4':
+            candidato.append(info['nm'])
+            votos.append(info['vap'])
+            porcentagem.append(info['pvap'])
 
-print(df)
-print('TOTAL DE URNAS:', procentagemUrnas,'%')
+    df = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=['Candidato', 'N votos', 'Porcentagem'])
+
+    print(df)
+    print('TOTAL DE URNAS:', procentagemUrnas,'%','\n', 'Hora da chegagem:', datetime.now().hour, ':', datetime.now().minute, '\n')
+
+    sleep(30)
